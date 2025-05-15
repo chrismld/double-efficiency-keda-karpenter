41fdbf89c167cb11424f923af8a8b19b7a5ea91b
Christian Melendez <christianhxc@gmail.com>
2025-05-15 20:34:05 +0100

    EKS and Karpenter templates

---
+++ karpenter/terraform/keda.tf
@@ -0,0 +1,15 @@
+resource "kubernetes_namespace" "keda" {
+  metadata {
+    name = "keda"
+  }
+}
+
+resource "helm_release" "keda" {
+  name       = "keda"
+  repository = "https://kedacore.github.io/charts"
+  chart      = "keda"
+  version    = local.keda_version
+  namespace  = "keda"
+
+  depends_on = [kubernetes_namespace.keda]
+}
